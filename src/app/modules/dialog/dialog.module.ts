ffe6d228862a72726eeed7c640c478b857b357de
ShadowUchiha <contrasena12345@contrasena12345s-Mac-mini.local>
2022-09-05 22:01:17 -0500

    cambios: - update angular

--- src/app/modules/dialog/dialog.module.ts
+++ src/app/modules/dialog/dialog.module.ts
@@ -4,8 +4,7 @@ import { DialogComponent } from './dialog.component'
 import { InsertionDirective } from './insertion.directive';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [DialogComponent, InsertionDirective],
-  entryComponents: [DialogComponent],
+    imports: [CommonModule],
+    declarations: [DialogComponent, InsertionDirective]
 })
 export class DialogModule {}
