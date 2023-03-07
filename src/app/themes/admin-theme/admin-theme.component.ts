c6de4fc12c7de3f9f8fb9a7a69e621691be177a4
Julio Cesar Tapia <jcesartap88@gmail.com>
2023-03-06 21:26:54 -0600

    cambios: - logout button

--- src/app/themes/admin-theme/admin-theme.component.ts
+++ src/app/themes/admin-theme/admin-theme.component.ts
@@ -1,9 +1,14 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {MenuObj} from '../../interfaces/menu.obj';
 import {MENU_CONFIG} from '../../constants/Menu';
 import {Store} from '@ngrx/store';
 import {State} from '../../interfaces/state.obj';
+import {UserInfo} from '../../models/UserInfo';
+import * as fromStore from '../../constants/ReduxConstants';
+import {Observable} from 'rxjs';
+import {deleteInfo} from '../../actions/userInfo.actions';
+import {RequestService} from '../../services/request/request.service';
 
 @Component({
   selector: 'admin-theme',
@@ -15,21 +20,59 @@ export class AdminThemeComponent implements OnInit {
   @Input() routeActive = '';
   @Input() showSideMenu = true;
   @Input() showTopMenu = true;
+  userInfoLogged: UserInfo;
+  title = '';
+  userSessionLogout = {name: 'Cerrar Sesión', value: 'Logout'};
 
   OptionsUser: Array<any> = [
-    {name: 'Test1', value: 'Test1'},
-    {name: 'Tes2', value: 'Test2' }
+    this.userSessionLogout,
   ];
 
-  constructor(private store: Store<State>, private route: ActivatedRoute) {
+  constructor(private store: Store<State>, private route: ActivatedRoute, private router: Router, private requestService: RequestService) {
   }
 
   ngOnInit(): void {
     const keyElement = 'element';
     this.routeActive = this.route.snapshot.data[keyElement];
+    this.getInfoUser();
+    this.title = this.userInfoLogged.name;
+  }
+
+  getInfoUser(){
+    this.store.select(fromStore.selectUserInfo).subscribe(
+      userInfo => {
+        this.userInfoLogged = userInfo;
+      }
+    );
   }
 
   selectedValue($event){
+    console.log($event.value);
+    if ($event.value === this.userSessionLogout.value){
+      this.logout();
+    }
+  }
+
+  logout(){
+    const userLogut: Observable<any> = this.requestService.logout({token: this.userInfoLogged.token});
+    const these = this;
+    userLogut.subscribe({
+      next(result){
+        const usr: UserInfo = {
+          name: '',
+          email: '',
+          token: ''
+        };
+        these.store.dispatch(deleteInfo({userInfo: null}));
+      },
+      error(error){
+        console.error(error);
+      },
+      complete(){
+        console.log('updated!!');
+        these.router.navigate(['/users/login']);
+      }
+    });
   }
 
 }
